9c111145a4d3977f073fb45d8f8692b308a170ac
Gabri739 <gabriele.bragaglia@outlook.it>
2025-07-15 17:38:52 +0200

    Ora Profilo Utente funziona e mostra info dell'utente corrente

--- src/main/java/it/uniroma3/siw/controller/UtenteController.java
+++ src/main/java/it/uniroma3/siw/controller/UtenteController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import it.uniroma3.siw.model.Credenziali;
 import it.uniroma3.siw.model.Utente;
 import it.uniroma3.siw.service.CredenzialiService;
 import it.uniroma3.siw.service.UtenteService;
@@ -31,8 +32,10 @@ public class UtenteController {
 			return "redirect:/login";
 		
 		Utente utenteCorrente = utenteService.getUtente();
-		
-		model.addAttribute("utente", utenteCorrente);
+		// Recupera anche le credenziali dell'utente corrente
+		Credenziali credenzialicorrenti = credenzialiService.getCredenziali(id);
+		model.addAttribute("utente", utenteCorrente); 
+		model.addAttribute("credenziali", credenzialicorrenti);
 		model.addAttribute("showPasswordModal", showPasswordModal);
 		return "utente/utente.html";
 	}
